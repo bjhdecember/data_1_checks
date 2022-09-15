df4d85a527838501bf3383871f74e27c9ddb995a
bjhdecember <barbarajh1202@yahoo.com>
2022-09-14 21:58:25 -0400

    sample test

---
+++ sample.py
@@ -0,0 +1,14 @@
+import numbers
+
+
+print("Hello World")
+
+words={'light', 'moon', 'sun'}
+print(words)
+
+cities_dict={'OH':'Cincinnati', 'KY':'Covington', 'IN':'Richmond'}
+print(cities_dict["OH"])
+
+data_tuple = {'world', 'data', 'donut'}
+print(tuple[0])
+
