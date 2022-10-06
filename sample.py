2c6b5f09311abf5a45a6b16a631c8512149f01d2
bjhdecember <barbarajh1202@yahoo.com>
2022-10-05 20:02:44 -0400

    Revised to update code

--- sample.py
+++ sample.py
@@ -3,12 +3,12 @@ import numbers
 
 print("Hello World")
 
-words={'light', 'moon', 'sun'}
-print(words)
+words=('light', 'moon', 'sun')
+print(words[0:1])
 
 cities_dict={'OH':'Cincinnati', 'KY':'Covington', 'IN':'Richmond'}
 print(cities_dict["OH"])
 
-data_tuple = {'world', 'data', 'donut'}
-print(tuple[0])
+data_tuple = ('world', 'data', 'donut')
+print(data_tuple[0:1])
 
